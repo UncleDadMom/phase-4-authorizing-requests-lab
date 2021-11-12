8feeca3008f573c48d37f17f1ac818ede05264b9
Patrick Wu <Ptrckwu@yahoo.com>
2021-11-12 12:06:12 -0500

    done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :record_not_found
+  before_action :authorize
 
   def index
     articles = Article.where(is_member_only: false).includes(:user).order(created_at: :desc)
@@ -15,6 +16,10 @@ class ArticlesController < ApplicationController
 
   def record_not_found
     render json: { error: "Article not found" }, status: :not_found
+  end
+
+  def authorize
+    return render json: { error: "Not authorized" }, status: :unauthorized unless session.include? :user_id
   end
 
 end
